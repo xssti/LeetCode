6fe9b64352432a8bc077e35d25cdbf1e7fcf648d
Shaista Syeda <89367457+xssti@users.noreply.github.com>
2022-04-08 11:17:17 -0400

    Time: 650 ms (34.86%), Space: 25.9 MB (73.10%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        nums1 = set(nums)
+        if len(nums1) < len(nums):
+            return True
+        return False
+    
+                    
+        
+        
+            
+                
+                
+        
