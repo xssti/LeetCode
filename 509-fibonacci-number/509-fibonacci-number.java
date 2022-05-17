ec97152871a7d207d8e0a1a8b6db9a0070b09f09
Shaista Syeda <89367457+xssti@users.noreply.github.com>
2022-05-17 09:05:12 -0400

    Time: 0 ms (100.00%), Space: 40.3 MB (69.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,9 +1,22 @@
 class Solution {
     public int fib(int n) {
+//         if(n<=1){
+//             return n;
+//         }
+      
+//             return fib(n-1)+fib(n-2);
+//     }
         if(n<=1){
             return n;
+        }else{
+        int n0 = 0, n1=1;
+        int result = 0;
+         for(int i=2; i<=n; i++){
+            result = n0 + n1;
+            n0 = n1;
+            n1 = result;
         }
-      
-            return fib(n-1)+fib(n-2);
-    }
+        return result;
+        }
+}
 }
