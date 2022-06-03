db1570714fd95955d45b219a71cba7fc1bde560f
Shaista Syeda <89367457+xssti@users.noreply.github.com>
2022-06-03 16:04:06 -0400

    Time: 14 ms (46.26%), Space: 69 MB (47.70%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,13 +1,20 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-       Map<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                return true;
-            }else{
-                map.put(nums[i],i);
-            }
+    //    Map<Integer,Integer> map = new HashMap<>();
+    //     for(int i=0;i<nums.length;i++){
+    //         if(map.containsKey(nums[i])){
+    //             return true;
+    //         }else{
+    //             map.put(nums[i],i);
+    //         }
+    //     }
+    //     return false;
+    // }
+    Set<Integer> set = new HashSet<>();
+        for(int num: nums){
+            if(set.contains(num)) return true;
+            set.add(num);
         }
         return false;
-    }
+}
 }
