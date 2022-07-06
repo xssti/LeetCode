fa77a43a553af0ec82e85f1cec4d29c679164ca0
Shaista Syeda <89367457+xssti@users.noreply.github.com>
2022-07-06 14:19:14 -0400

    Time: 12 ms (43.38%), Space: 104.3 MB (26.04%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode mid = middleNode(head);
+        ListNode reverse = reverseList(mid); //reverse ListNode starting from middle
+        while(reverse!=null) {
+            if(reverse.val != head.val) 
+                return false;
+            reverse = reverse.next;
+            head = head.next;
+        }
+        return true;    
+    }
+    
+    //Reverse ListNode
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null; // first pointer
+        ListNode curr = head; // second pointer
+        while(curr!=null) {
+            ListNode next = curr.next; // third pointer
+            curr.next = prev; // reversing the pointer
+            prev = curr;  
+            curr = next;  // moving forward using next
+        }
+        return prev;
+    }
+    
+    //Find middle of ListNode
+    public static ListNode middleNode(ListNode head) {
+        ListNode fast = head, slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
