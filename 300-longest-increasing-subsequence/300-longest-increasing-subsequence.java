aac18dcfcd702c285a927c61ae22e9976a9fb9a6
Shaista Syeda <89367457+xssti@users.noreply.github.com>
2022-08-08 13:27:11 -0400

    Time: 9 ms (78.99%), Space: 42.2 MB (87.38%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,16 +1,15 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int result = 1;
-        int[] dp = new int[nums.length+1];
-        Arrays.fill(dp, 1);
-        for(int i=0; i<nums.length; i++) {
-            for(int j=0; j<i; j++) {
-                if(nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j]+1);
-                    result = Math.max(result, dp[i]);
-                }
+        TreeSet<Integer> bst = new TreeSet<>();
+        for(int num: nums) {
+            Integer higherOrequal = bst.ceiling(num);
+            if(higherOrequal == null) {
+                bst.add(num);
+            } else {
+                bst.remove(higherOrequal);
+                bst.add(num);
             }
         }
-        return result;
+        return bst.size();
     }
 }
