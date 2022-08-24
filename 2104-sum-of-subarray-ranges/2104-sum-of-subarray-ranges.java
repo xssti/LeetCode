36ca5a963ef4e0bd84504153069090603fd63bba
Shaista Syeda <89367457+xssti@users.noreply.github.com>
2022-08-24 00:07:24 -0400

    Time: 40 ms (20.65%), Space: 48 MB (14.03%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -0,0 +1,33 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+        return getTotal(nums, n, true) - getTotal(nums, n, false);
+       
+    }
+    public long getTotal(int[] nums, int n, boolean isMax) {
+        long total = 0;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i=0; i<n; i++) {
+            while(!st.isEmpty() && (isMax ? nums[st.peek()] <= nums[i] : nums[st.peek()] >= nums[i])) {
+                st.pop();
+            }
+            left[i] = st.isEmpty() ? -1 : st.peek();
+            st.add(i);
+        }
+        st = new Stack<>();
+        for(int i=n-1; i>=0; i--) {
+            while(!st.isEmpty() && (isMax ? nums[st.peek()] < nums[i] : nums[st.peek()] > nums[i])) {
+                st.pop();
+            }
+            right[i] = st.isEmpty() ? n : st.peek();
+            st.add(i);
+        }
+        for(int i=0; i<n; i++) {
+            total += (long) (i-left[i])*(right[i]-i)*nums[i];
+        }
+        return total;
+    }
+}
+   
