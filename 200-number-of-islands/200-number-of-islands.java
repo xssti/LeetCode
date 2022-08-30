c28773c14f7e5453e7fc86d60ff70dd40589d1f1
Shaista Syeda <89367457+xssti@users.noreply.github.com>
2022-08-30 11:37:00 -0400

    Time: 3 ms (97.79%), Space: 57.9 MB (22.76%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -5,18 +5,18 @@ class Solution {
             for(int j=0; j<grid[i].length; j++) {
                 if(grid[i][j] == '1') {
                     islands++;
-                    callBFS(grid, i, j);
+                    callDFS(grid, i, j);
                 }
             }
         }
         return islands;
     }
-    public void callBFS(char[][] grid, int i, int j) {
+    public void callDFS(char[][] grid, int i, int j) {
         if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') return;
         grid[i][j] = '0';
-        callBFS(grid, i+1, j);
-        callBFS(grid, i-1, j);
-        callBFS(grid, i, j-1);
-        callBFS(grid, i, j+1);
+        callDFS(grid, i+1, j);
+        callDFS(grid, i-1, j);
+        callDFS(grid, i, j-1);
+        callDFS(grid, i, j+1);
     }
 }
