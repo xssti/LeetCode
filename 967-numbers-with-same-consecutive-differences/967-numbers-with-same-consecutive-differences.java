0be8f0bb14377b346777a6f6f39932feb872b4b0
Shaista Syeda <89367457+xssti@users.noreply.github.com>
2022-09-03 15:44:42 -0400

    Time: 1 ms (100.00%), Space: 43.1 MB (44.80%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,32 +1,24 @@
 class Solution {
     public int[] numsSameConsecDiff(int N, int K) {
-        if (N == 1)
-            return new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        List<Integer> results = new ArrayList<Integer>();
-        for (int num = 1; num < 10; ++num)
-            this.DFS(N - 1, num, K, results);
-
-        // convert the ArrayList to int[]
-        return results.stream().mapToInt(i->i).toArray();
+        List<Integer> list = new ArrayList<>();
+        for(int i=1; i<10; i++) {
+            recurse(N - 1, K, i, list);
+        }
+        int[] ans = new int[list.size()];
+        for(int i=0; i<list.size(); i++) ans[i] = list.get(i);
+        return ans;
     }
-
-    protected void DFS(int N, int num, int K, List<Integer> results) {
-        if (N == 0) {
-            results.add(num);
+    public void recurse(int n, int k, int curr, List<Integer> list) {
+        if(n == 0) {
+            list.add(curr);
             return;
         }
-        List<Integer> nextDigits = new ArrayList<>();
-
-        Integer tailDigit = num % 10;
-        nextDigits.add(tailDigit + K);
-        if (K != 0)
-            nextDigits.add(tailDigit - K);
-        for (Integer nextDigit : nextDigits) {
-            if (0 <= nextDigit && nextDigit < 10) {
-                Integer newNum = num * 10 + nextDigit;
-                this.DFS(N - 1, newNum, K, results);
-            }
+        int lastdigit =  curr % 10;
+        if(lastdigit - k >= 0) {
+            recurse(n-1, k, curr*10 + lastdigit-k, list);
+        }
+        if(k !=0 && lastdigit+k <= 9){
+            recurse(n-1, k, curr*10 + lastdigit + k, list);
         }
     }
 }
