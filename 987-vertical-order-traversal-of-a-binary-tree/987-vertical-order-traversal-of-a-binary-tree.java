588c05e323ab3bae87423090d2a5fc2d52172fbf
Shaista Syeda <89367457+xssti@users.noreply.github.com>
2022-09-03 21:59:31 -0400

    Time: 7 ms (31.20%), Space: 43.6 MB (15.96%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class QNode {
+    TreeNode n;
+    int level;
+    
+    public QNode(TreeNode n, int level) {
+        this.n = n;
+        this.level = level;
+    }
+}
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        TreeMap<Integer,List<Integer>> map = bfs(root);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int key : map.keySet()) {
+            ans.add(map.get(key));
+        }
+        return ans;
+    }
+    
+    public TreeMap<Integer,List<Integer>> bfs(TreeNode node) {
+        TreeMap<Integer,List<Integer>> map = new TreeMap<>();
+        Queue<QNode> q = new LinkedList<>();
+        q.add(new QNode(node, 0));
+        
+        while(!q.isEmpty()) {
+            QNode[] arr = new QNode[q.size()];
+            for(int i=0; i<arr.length; i++) {
+                QNode qn = q.remove();
+                if(qn.n.left != null) q.add(new QNode(qn.n.left, qn.level - 1));
+                if(qn.n.right != null) q.add(new QNode(qn.n.right, qn.level + 1));
+                arr[i] = qn;
+            }
+            Arrays.sort(arr, (a, b) -> {
+                if(a.level == b.level) return a.n.val - b.n.val;
+                return a.level - b.level;
+            });
+            for(int i=0; i<arr.length; i++) {
+                List<Integer> list = map.getOrDefault(arr[i].level, new ArrayList<>());
+                list.add(arr[i].n.val);
+                map.put(arr[i].level, list);
+            }
+        }
+        return map;
+    }
+}
