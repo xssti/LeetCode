9269866c535cf2ae9e3a510caa8a90ee4dc530d0
Shaista Syeda <89367457+xssti@users.noreply.github.com>
2022-10-11 15:46:46 -0400

    Time: 4 ms (78.08%), Space: 49.9 MB (44.13%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,14 +1,33 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        List<Integer> list = new ArrayList<>();
-        for(int num : nums1)
-            list.add(num);
-        for(int num : nums2) list.add(num);
-        Collections.sort(list);
-        System.out.println(list);
-        int mid = list.size()/2;
-        if(list.size() % 2 != 0) return list.get(mid);
-        double num  = (double)(list.get(mid - 1) + list.get(mid)) / 2 ;
-        return num;
+         if(nums2.length < nums1.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        
+        int[] smallArray = nums1, bigArray = nums2;
+        int total = smallArray.length + bigArray.length;
+        int half = total / 2;
+        
+        int left = 0, right = smallArray.length - 1;
+        while(true) {
+            int smallPointer = Math.floorDiv(right+left,2);
+            int bigPointer = half - smallPointer - 2;
+            
+            int smallLeft = smallPointer < 0 ? Integer.MIN_VALUE : smallArray[smallPointer];
+            int smallRight = smallPointer+1 >= smallArray.length ? Integer.MAX_VALUE : smallArray[smallPointer+1];
+            int bigLeft = bigPointer < 0 ? Integer.MIN_VALUE : bigArray[bigPointer];
+            int bigRight = bigPointer+1 >= bigArray.length ? Integer.MAX_VALUE : bigArray[bigPointer+1];
+            
+            if(smallLeft <= bigRight && bigLeft <= smallRight) {
+                if(total % 2 != 0) {
+                    return Math.min(smallRight, bigRight);
+                }
+                return (double) ((Math.max(smallLeft, bigLeft) + Math.min(smallRight, bigRight))) / 2;
+            } else if(smallLeft > bigRight) {
+                right = smallPointer - 1;
+            } else {
+                left = smallPointer + 1;
+            }
+        }
     }
 }
